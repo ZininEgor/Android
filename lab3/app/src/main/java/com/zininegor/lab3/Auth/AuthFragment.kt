d2fdf6a938067cb60607c7883cd14f30b3626952
ZininEgor <zininegor00@gmail.com>
2021-01-12 23:43:57 +0300

    .DS_Store banished!

---
+++ lab3/app/src/main/java/com/zininegor/lab3/Auth/AuthFragment.kt
@@ -0,0 +1,11 @@
+package com.zininegor.lab3.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AuthFragment: Fragment() {
+
+}
---
+++ lab3/app/src/main/java/com/zininegor/lab3/Auth/AuthViewModel.kt
@@ -0,0 +1,2 @@
+package com.zininegor.lab3.Auth
+
